8297bb10d090bad0e9e0706653431289d8d582d1
Angelo B <realanquilis@gmail.com>
2020-11-14 20:00:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//TODO create a index of projects, will make it easier to change what's on portfolio
+
+let projects = {
+  projectName: [],
+  projectPicture: [],
+  projectDesc: [],
+  projectPos: ["1","2","3","4","5"]
+}
+
+//TODO auto-update contactInfo links 
+
+let contactInfo = {
+  email: "",
+  twitter: "",
+  gitHub: "",
+  resume: ""
+}
